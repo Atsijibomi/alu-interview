eba35cc04973219dd380340edd4a87d537862024
Athanson Sijibomi <o.athanson@alustudent.com>
2023-09-13 06:41:28 -0700

    Minimum Operations

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Interview question on minimum operation
+"""
+
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+    operations = 0
+    for i in range(2, n+1):
+        while n % i == 0:
+            operations += i
+            n /= i
+        if n == 1:
+            return operations
+    return 0
